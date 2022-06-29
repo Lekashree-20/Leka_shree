a50646a7bc81d023260c7ac1f5a9b6ee1307a1b8
Lekashree-20 <95126942+Lekashree-20@users.noreply.github.com>
2022-06-29 07:17:21 -0700

    Add files via upload

---
+++ remove set.ipynb.py
@@ -0,0 +1,11 @@
+# language set
+language = {'English', 'French', 'German'}
+print(language)
+
+# removing 'German' from language
+language.remove('German')
+
+
+# Updated language set
+print('Updated language set:', language)
+print("BY LEKA SHREE")
