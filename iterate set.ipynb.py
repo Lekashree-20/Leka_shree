13234716002a3a5e92509beec0134f97eaa9c42a
Lekashree-20 <95126942+Lekashree-20@users.noreply.github.com>
2022-06-29 06:50:46 -0700

    Add files via upload

---
+++ iterate set.ipynb.py
@@ -0,0 +1,7 @@
+# Creating a set using string
+test_set = set("leka")
+
+# Iterating using for loop
+for val in test_set:
+    print(val)
+print("BY LEKA SHREE")
---
+++ size dict.ipynb.py
@@ -0,0 +1,11 @@
+# sample Dictionaries
+dic1 = {"A": 1, "B": 2, "C": 3}
+dic2 = {"abc": "Raju", "def": "Nikhil", "ghi": "Deepanshu"}
+dic3 = {1: "Lion", 2: "Tiger", 3: "Fox", 4: "Wolf"}
+
+# print the sizes of sample Dictionaries
+print("Size of dic1: " + str(dic1.__sizeof__()) + "bytes")
+print("Size of dic2: " + str(dic2.__sizeof__()) + "bytes")
+print("Size of dic3: " + str(dic3.__sizeof__()) + "bytes")
+
+print("BY LEKA SHREE")
